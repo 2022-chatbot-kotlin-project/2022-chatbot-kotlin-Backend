7fedef180127e22a4bdc266f3d2548f73cbc1989
hee <jkgt0521@naver.com>
2022-08-20 15:23:52 +0900

    Feat: #4 프로젝트 모듈 분리 및 프로젝트 구조 설정

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,18 +1,8 @@
 from flask.cli import FlaskGroup
-from project import app, db, User
+from project import app
 
 cli = FlaskGroup(app)
 
-@cli.command("create_db")
-def create_db():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-
-@cli.command("seed_db")
-def seed_db():
-    db.session.add(User(email="test@db.com"))
-    db.session.commit()
 
 if __name__ == "__main__":
     cli()
