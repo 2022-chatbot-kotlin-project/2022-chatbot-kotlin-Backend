cb6944be0f4a5013f6742ec7a5a45011ddd4eaf5
hongbi-lee <foxgirin950@gmail.com>
2022-09-19 19:47:59 +0900

    Feat: #14 API 수정

--- project/__init__.py
+++ project/__init__.py
@@ -5,8 +5,10 @@ app.config.from_object("project.config.Config")
 
 from project.chatbot.chatbot import bp as chatbot_module
 from project.views.map import bp as map_module
+from project.views.search import bp as search_module
 # register Blueprint
 app.register_blueprint(map_module)
+app.register_blueprint(search_module)
 app.register_blueprint(chatbot_module)
 
 
--- project/views/map.py
+++ project/views/map.py
@@ -1,17 +1,15 @@
-from flask import Blueprint, request, jsonify
+import json
+from flask import Blueprint
 from project.models.shelter import Shelter
 
-bp = Blueprint('views', __name__, url_prefix='/map')
+bp = Blueprint('map', __name__, url_prefix='/map')
 
 
-@bp.route("/<type>")    # 향후 views.py로 모듈 분리
-def shelter_search(type):
-    print("type : ", type)
-    qry = Shelter.query.filter_by(type=type).all()
-    print(len(qry))
+@bp.route("/all")    # 향후 views.py로 모듈 분리
+def get_all_shelters():
+    qry = Shelter.query.all()
     data = [{"index": q.idx, "region": q.addr, "name": q.name, "address": q.addr, "tel1": q.phone1, "tel2": q.phone2,
              "target": q.target, "category": q.type, "people": q.limit, "url": q.url,
              "latitude": q.lat, "longitude": q.lng} for q in qry]
-    print(data)
-    return jsonify(data)
+    return json.dumps({'status': 200, 'length': len(data), 'data': data}), 200
 
